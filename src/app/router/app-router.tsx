33863ebde1ccf0e7bf4b08e77fe80fcacd20dc5a
maskhar <bimokharis1810@gmail.com>
2026-09-12 19:49:01 +0700

    Rapikan format route privasi

--- src/app/router/app-router.tsx
+++ src/app/router/app-router.tsx
@@ -6,7 +6,7 @@ import { WorkspaceEntryRedirect, WorkspaceGuard } from '@/features/workspaces/wo
 
 const PublicHomePage = lazy(() => import('@/pages/public/public-home-page').then((module) => ({ default: module.PublicHomePage })));
 const Home02Page = lazy(() => import('@/pages/public/home-02-page').then((module) => ({ default: module.Home02Page })));
-const PrivacyPage = lazy(() => import('@/pages/public/privacy-page').then((module) => ({ default: module.PrivacyPage }))); 
+const PrivacyPage = lazy(() => import('@/pages/public/privacy-page').then((module) => ({ default: module.PrivacyPage })));
 const LoginPage = lazy(() => import('@/pages/public/login-page').then((module) => ({ default: module.LoginPage })));
 const AccountPasswordPage = lazy(() => import('@/pages/public/account-password-page').then((module) => ({ default: module.AccountPasswordPage })));
 const CampaignRequestPage = lazy(() => import('@/pages/public/campaign-request-page').then((module) => ({ default: module.CampaignRequestPage })));
