2093555dd4c2eece82582006244434c30d6c0b33
maskhar <bimokharis1810@gmail.com>
2026-09-12 20:47:53 +0700

    Tambah header dan footer database

--- src/pages/public/network-database-page.tsx
+++ src/pages/public/network-database-page.tsx
@@ -4,5 +4,5 @@ import { PublicNetworkPreview } from '@/features/network/public-network-preview'
 import './network-database-page.css';
 
 export function NetworkDatabasePage() {
-  return <main className="network-database-page"><header><Link to="/home-02"><img src={headerLogo02} alt="Buzzerhood" /></Link><Link to="/home-02">Kembali ke Beranda</Link></header><PublicNetworkPreview /></main>;
+  return <main className="network-database-page"><header><Link to="/home-02"><img src={headerLogo02} alt="Buzzerhood" /></Link><nav><a href="/home-02#beranda-02">Beranda</a><a href="/home-02#layanan-02">Layanan</a><a href="/home-02#paket-02">Paket</a><a href="/home-02#jaringan-02">Jaringan</a><a href="/home-02#studi-kasus-02">Studi Kasus</a><a href="/home-02#tentang-02">Tentang</a></nav><Link to="/home-02">Kembali ke Beranda</Link></header><PublicNetworkPreview /><footer><div><img src={headerLogo02} alt="Buzzerhood" /><span>Ideas People Impact</span></div><nav><b>Menu</b><a href="/home-02#beranda-02">Beranda</a><a href="/home-02#layanan-02">Layanan</a><a href="/home-02#paket-02">Paket</a><a href="/home-02#jaringan-02">Jaringan</a></nav><div><b>Hubungi Kami</b><a href="mailto:hallo@buzzerhood.com">hallo@buzzerhood.com</a><a href="/privacy">Privasi</a><a href="/terms">Syarat &amp; Ketentuan</a></div></footer></main>;
 }
